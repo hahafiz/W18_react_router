cba190a3d4581ed19fe3ae9641ba64a28a8128d7
hahafiz <hafizqamarul19@gmail.com>
2025-02-16 10:25:08 +0800

    useSearchParams

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import Header from "../componenets/Header";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
+  const [searchParams] = useSearchParams();
+
+  const name = searchParams.get("name");
+
   return (
     <>
       <Header />
-      <div>Home</div>
+      <div>Home Sweet Home, {name}</div>
     </>
   );
 };
